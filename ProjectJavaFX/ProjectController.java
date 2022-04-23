0d3d063f8146fe6f847f32f7822c0b900e5685d7
Jeancdb <jean.constantdb@gmail.com>
2022-04-23 18:10:02 +0200

    Tentative d'entré de valeurs dans les cellules de tableView
    Échec

--- ProjectJavaFX/ProjectController.java
+++ ProjectJavaFX/ProjectController.java
@@ -12,6 +12,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.collections.*;
 import java.util.*;
+import javafx.beans.property.SimpleStringProperty;
 
 /**
  * Controller for the Project.
@@ -33,6 +34,8 @@ public class ProjectController
     private SplitMenuButton promotion;
     @FXML
     private TableView tableView;
+    @FXML
+    private TableColumn nameT,monthT,timeT,subjectT,promoT;
      
     /**
      * To Add a new row in the TableView
@@ -42,12 +45,12 @@ public class ProjectController
     {
         // Counts number of button clicks and shows the result on a label
         rightTitle.setText("Ajout d'un stage,\nveuillez remplir les champs");
+        
         ObservableList<Map> allData = tableView.getItems();
         int offset = allData.size();
         Map<String, String> dataRow = new HashMap<>();
         for (int j = 0; j < tableView.getColumns().size(); j++) {
             String mapKey = Character.toString((char) ('A' + j));
-            System.out.println(mapKey);
             String value1 = mapKey + (offset + 1);
             System.out.println(value1);
             dataRow.put(mapKey,value1);
@@ -55,6 +58,16 @@ public class ProjectController
         }
         System.out.println(dataRow);
         allData.add(dataRow);
+        
+        List<TableColumnBase> columns = tableView.getColumns();
+        for(TableColumnBase e: columns) {
+            tableView.getColumns().add((TableColumn<T, ?>) e);
+        }
+        for (int i = 0; i < tableView.getColumns().size(); i++) {
+            ((TableColumn)(tableView.getColumns().get(i))).setVisible(false);
+            
+            ((TableColumn)(tableView.getColumns().get(i))).setVisible(true);
+        }
         
     }
     
