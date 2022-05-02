38833fa1ae169e1a366317c93bd3daf3092ba6fd
QuentinS98 <qsimon0498@gmail.com>
2022-05-02 12:40:24 +0200

    Connection
    Connection BDD Pas fini

---
+++ ProjectJavaFX/lib/sqlite-jdbc/src/test/java/org/sqlite/SQLiteConfigTest.java
@@ -0,0 +1,32 @@
+package org.sqlite;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+import java.util.Properties;
+import org.junit.jupiter.api.Test;
+
+public class SQLiteConfigTest {
+
+    @Test
+    public void toProperites() throws SQLException {
+        SQLiteConfig config = new SQLiteConfig();
+
+        config.setReadOnly(true);
+        config.setDateStringFormat("yyyy/mm/dd");
+        config.setDatePrecision("seconds");
+        config.setDateClass("real");
+
+        Properties properties = config.toProperties();
+
+        assertEquals(
+                "yyyy/mm/dd",
+                properties.getProperty(SQLiteConfig.Pragma.DATE_STRING_FORMAT.getPragmaName()));
+        assertEquals(
+                SQLiteConfig.DatePrecision.SECONDS.name(),
+                properties.getProperty(SQLiteConfig.Pragma.DATE_PRECISION.getPragmaName()));
+        assertEquals(
+                SQLiteConfig.DateClass.REAL.name(),
+                properties.getProperty(SQLiteConfig.Pragma.DATE_CLASS.getPragmaName()));
+    }
+}
---
+++ ProjectJavaFX/lib/sqlite-jdbc/src/test/java/org/sqlite/util/OSInfoTest.java
@@ -0,0 +1,149 @@
+// --------------------------------------
+// sqlite-jdbc Project
+//
+// OSInfoTest.java
+// Since: May 20, 2008
+//
+// $URL$
+// $Author$
+// --------------------------------------
+package org.sqlite.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.SetSystemProperty;
+
+public class OSInfoTest {
+    private static final Logger logger = Logger.getLogger(OSInfoTest.class.getName());
+
+    @Test
+    public void osName() {
+        assertEquals("Windows", OSInfo.translateOSNameToFolderName("Windows XP"));
+        assertEquals("Windows", OSInfo.translateOSNameToFolderName("Windows 2000"));
+        assertEquals("Windows", OSInfo.translateOSNameToFolderName("Windows Vista"));
+        assertEquals("Windows", OSInfo.translateOSNameToFolderName("Windows 98"));
+        assertEquals("Windows", OSInfo.translateOSNameToFolderName("Windows 95"));
+
+        assertEquals("Mac", OSInfo.translateOSNameToFolderName("Mac OS"));
+        assertEquals("Mac", OSInfo.translateOSNameToFolderName("Mac OS X"));
+
+        assertEquals("AIX", OSInfo.translateOSNameToFolderName("AIX"));
+
+        assertEquals("Linux", OSInfo.translateOSNameToFolderName("Linux"));
+        assertEquals("OS2", OSInfo.translateOSNameToFolderName("OS2"));
+
+        assertEquals("HPUX", OSInfo.translateOSNameToFolderName("HP UX"));
+    }
+
+    @Test
+    public void archName() {
+        assertEquals("i386", OSInfo.translateArchNameToFolderName("i386"));
+        assertEquals("x86", OSInfo.translateArchNameToFolderName("x86"));
+        assertEquals("ppc", OSInfo.translateArchNameToFolderName("ppc"));
+        assertEquals("amd64", OSInfo.translateArchNameToFolderName("amd64"));
+    }
+
+    @Test
+    public void folderPath() {
+        String[] component = OSInfo.getNativeLibFolderPathForCurrentOS().split("/");
+        assertEquals(2, component.length);
+        assertEquals(OSInfo.getOSName(), component[0]);
+        assertEquals(OSInfo.getArchName(), component[1]);
+    }
+
+    @Test
+    public void testMainForOSName() throws Exception {
+
+        // preserve the current System.out
+        PrintStream out = System.out;
+        try {
+            // switch STDOUT
+            ByteArrayOutputStream buf = new ByteArrayOutputStream();
+            PrintStream tmpOut = new PrintStream(buf);
+            System.setOut(tmpOut);
+            OSInfo.main(new String[] {"--os"});
+            assertEquals(OSInfo.getOSName(), buf.toString());
+        } finally {
+            // reset STDOUT
+            System.setOut(out);
+        }
+    }
+
+    @Test
+    public void testMainForArchName() throws Exception {
+
+        // preserver the current System.out
+        PrintStream out = System.out;
+        try {
+            // switch STDOUT
+            ByteArrayOutputStream buf = new ByteArrayOutputStream();
+            PrintStream tmpOut = new PrintStream(buf);
+            System.setOut(tmpOut);
+            OSInfo.main(new String[] {"--arch"});
+            assertEquals(OSInfo.getArchName(), buf.toString());
+        } finally {
+            // reset STDOUT
+            System.setOut(out);
+        }
+    }
+
+    @Test
+    public void testGetHardwareName() throws Exception {
+        String hardware = OSInfo.getHardwareName();
+        logger.info("Hardware name: " + hardware);
+    }
+
+    // it's unlikely we run tests on an Android device
+    @Test
+    void testIsNotAndroid() {
+        assertFalse(OSInfo.isAndroid());
+    }
+
+    @Nested
+    @SetSystemProperty(key = "java.runtime.name", value = "Java for Android")
+    @SetSystemProperty(key = "os.name", value = "Linux for Android")
+    class Android {
+        @Test
+        public void testIsAndroid() {
+            assertTrue(OSInfo.isAndroid());
+        }
+
+        @Test
+        @SetSystemProperty(key = "os.arch", value = "arm")
+        public void testArmvNativePath() throws IOException, InterruptedException {
+            try {
+                ProcessRunner mockRunner = mock(ProcessRunner.class);
+                OSInfo.processRunner = mockRunner;
+                when(mockRunner.runAndWaitFor("uname -m")).thenReturn("armv7l");
+
+                assertEquals("Linux-Android/arm", OSInfo.getNativeLibFolderPathForCurrentOS());
+            } finally {
+                OSInfo.processRunner = new ProcessRunner();
+            }
+        }
+
+        @Test
+        @SetSystemProperty(key = "os.arch", value = "arm64")
+        public void testArm64NativePath() throws IOException, InterruptedException {
+            try {
+                ProcessRunner mockRunner = mock(ProcessRunner.class);
+                OSInfo.processRunner = mockRunner;
+                when(mockRunner.runAndWaitFor("uname -m")).thenReturn("aarch64");
+
+                assertEquals("Linux-Android/aarch64", OSInfo.getNativeLibFolderPathForCurrentOS());
+            } finally {
+                OSInfo.processRunner = new ProcessRunner();
+            }
+        }
+    }
+}
